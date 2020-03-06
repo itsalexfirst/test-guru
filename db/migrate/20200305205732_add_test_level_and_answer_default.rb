68dcc3bb080c7a541b975718512d88a0fa4dd990
itsalexfirst <itsalex@yandex.ru>
2020-03-06 21:08:52 +0300

    references

--- db/migrate/20200305205732_add_test_level_and_answer_default.rb
+++ db/migrate/20200305205732_add_test_level_and_answer_default.rb
@@ -1,6 +1,6 @@
 class AddTestLevelAndAnswerDefault < ActiveRecord::Migration[6.0]
   def change
     change_column_default(:tests, :level, from: 0, to: 1)
-    change_column_default(:questions, :body, from: true, to: false)
+    change_column_default(:answers, :correct, from: true, to: false)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_205732) do
+ActiveRecord::Schema.define(version: 2020_03_06_174650) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "questions_id"
+    t.index ["questions_id"], name: "index_answers_on_questions_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -25,9 +27,11 @@ ActiveRecord::Schema.define(version: 2020_03_05_205732) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body", default: "f", null: false
+    t.string "body", default: "t", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "tests_id"
+    t.index ["tests_id"], name: "index_questions_on_tests_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -35,6 +39,8 @@ ActiveRecord::Schema.define(version: 2020_03_05_205732) do
     t.integer "level", default: 1, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "categories_id"
+    t.index ["categories_id"], name: "index_tests_on_categories_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +50,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_205732) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions", column: "questions_id"
+  add_foreign_key "questions", "tests", column: "tests_id"
+  add_foreign_key "tests", "categories", column: "categories_id"
 end
