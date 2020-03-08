c7e226bc5c9a03429c9f76ec1553cd04d3a304ef
itsalexfirst <itsalex@yandex.ru>
2020-03-08 04:18:28 +0300

    all tests by cetegory

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.all_by_category(category)
+    category_id = Category.find_by(title: category).id
+    Test.where(category: category_id).orfer('id DESC').pluck(:title)
+  end
 end
