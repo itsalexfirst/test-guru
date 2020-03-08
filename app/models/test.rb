3c8b6ddbd3c2cab56ca17807a3c1f649056caaa3
itsalexfirst <itsalex@yandex.ru>
2020-03-08 21:36:13 +0300

    joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
 
   def self.all_by_category(category)
     category_id = Category.find_by(title: category).id
-    Test.where(category: category_id).orfer('id DESC').pluck(:title)
+    Test.where(category: category_id).order('id DESC').pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 class User < ApplicationRecord
 
   def passed_tests(level)
-    test_by_level = []
-    PassedTest.where(user_id: self.id).each do |passed_test|
-      id = passed_test.test_id
-      test = Tests.find(id)
-      test_by_level << test.title if test.level == level
-    end
-    test_by_level
+    PassedTest.where('user_id = ?', self.id).joins(:test).where('tests.level = ?', level)
   end
 end
