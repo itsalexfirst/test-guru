a40b067b863069e231567324157bb1f34750afe6
itsalexfirst <itsalex@yandex.ru>
2020-03-10 01:48:01 +0300

    author

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  has_many :users, through: :passed_tests
+  belongs_to :author, class_name: 'User'
 
   def self.all_by_category(category)
     Test.joins(:category).where('categories.title = ?', category).order('tests.id DESC').pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tests, through: :passed_tests
+  has_many :added_tests , class_name: 'Test', foreign_key: 'author_id'
 
   def passed_tests(level)
     PassedTest.where('user_id = ?', self.id).joins('INNER JOIN "tests" ON "tests"."id" = "passed_tests"."test_id"').where('tests.level = ?', level)
