ea40c834d7eed7b29bc18c5f4966ed831977764f
itsalexfirst <itsalex@yandex.ru>
2020-03-15 22:41:29 +0300

    scope 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,5 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-
-  def self.all_by_category(category)
-    Test.joins(:category).where('categories.title = ?', category).order('tests.id DESC').pluck(:title)
-  end
+  scope :all_by_category, -> (category) { joins(:category).where(categories: { title: category }) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :passed_tests
   has_many :added_tests , class_name: 'Test', foreign_key: 'author_id'
 
-  def passed_tests(level)
-    PassedTest.where('user_id = ?', self.id).joins('INNER JOIN "tests" ON "tests"."id" = "passed_tests"."test_id"').where('tests.level = ?', level)
+  def tests_by_level(level)
+    tests.where(level: level)
   end
 end
