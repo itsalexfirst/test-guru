df80f621ee242bb2e0196104cc4d9ef46a676acf
itsalexfirst <itsalex@yandex.ru>
2020-03-23 02:29:52 +0300

    test:create&update

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,4 @@
 class TestsController < ApplicationController
-  #before_action :find_test, only: [:show]
 
   def index
     @tests = Test.all
@@ -9,9 +8,41 @@ class TestsController < ApplicationController
     @test = find_test
   end
 
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
   private
 
   def find_test
     Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
