20ef50b1b55e9a1bfdc2ff4fd9e2b17fd48514af
itsalexfirst <itsalex@yandex.ru>
2020-03-23 22:28:17 +0300

    footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}"
+  end
 end
+
+
+
