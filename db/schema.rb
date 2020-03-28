5250cdcc3f05172d3e2fc50facdf26a4b1d08d6b
itsalexfirst <itsalex@yandex.ru>
2020-03-29 01:15:27 +0300

    users current_question

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_201148) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "current_questions"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
