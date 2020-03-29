b23d9963f08fa82ea3c9f8c078748e7bcd14c972
itsalexfirst <itsalex@yandex.ru>
2020-03-30 00:23:55 +0300

    user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :added_tests , class_name: 'Test', foreign_key: 'author_id'
-
-  validates :name, presence: true
-  validates :email, presence: true
 
   def tests_by_level(level)
     tests.where(level: level)
@@ -14,15 +14,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  private
-
-  def digest
-    Digest::SHA1.hexdigest(string)
   end
 end
