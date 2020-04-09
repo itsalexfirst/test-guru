1996e3078949ef297697c83fc7aa286921c7bc1a
itsalexfirst <itsalex@yandex.ru>
2020-04-10 02:02:57 +0300

    add sorting tests by title

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/utilities/sorting")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,3 +20,4 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
