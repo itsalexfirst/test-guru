4e717d3c4d058ab4dd9b8e382f9de9ff6bd7f37e
itsalexfirst <itsalex@yandex.ru>
2020-04-28 19:29:25 +0300

    badge by level

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -16,15 +16,19 @@ class BadgeService
 
   def category?(badge)
     return if @user.badges.include?(badge)
-    (Test.all_by_category(badge.rule_value).ids - @user.test_passages.select{ |x| x.success? }.map(&:test_id)).empty?
+    (Test.all_by_category(badge.rule_value).ids - success_passed_tests).empty?
   end
 
   def level?(badge)
-
+    return if @user.badges.include?(badge)
+    (Test.all_by_level(badge.rule_value.to_i).ids - success_passed_tests).empty?
   end
 
   def attempt?(badge)
     @user.tests.where(id: @test.id).count == badge.rule_value.to_i
   end
 
+  def success_passed_tests_id
+    @user.test_passages.select{ |x| x.success? }.map(&:test_id)
+  end
 end
